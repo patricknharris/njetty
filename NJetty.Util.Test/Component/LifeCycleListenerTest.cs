9d8cbe4668185f48e6bec370d95a2725ca3e2db1
leopoldo.agdeppa <leopoldo.agdeppa@39fb20ce-a4f3-11dd-9b86-41075523de00>
2008-11-14 09:58:15 +0000

    NUnit Tests Added

---
+++ NJetty.Util.Test/Component/LifeCycleListenerTest.cs
@@ -0,0 +1,140 @@
+﻿#region Apache License
+//
+// Licensed to the Apache Software Foundation (ASF) under one or more 
+// contributor license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership. 
+// The ASF licenses this file to you under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NJetty.Util.Logger;
+
+namespace NJetty.Util.Test.Component
+{
+
+
+    /// <summary>
+    /// Test For Component
+    /// </summary>
+    /// <author>  
+    ///     <a href="mailto:leopoldo.agdeppa@gmail.com">Leopoldo Lee Agdeppa III</a>
+    /// </author>
+    /// <date>
+    /// November 2008
+    /// </date>
+
+
+
+    [TestFixture]
+    public class LifeCycleListenerTest
+    {
+
+        static Exception cause = new Exception("expected test exception");
+
+        
+
+        [Test]
+        public void TestStart()
+        {
+            TestLifeCycle lifecycle = new TestLifeCycle();
+            TestListener listener = new TestListener();
+            lifecycle.AddLifeCycleListener(listener);
+            ((StdErrLog)Log.Logger).HideStacks = true;
+            lifecycle.Cause = cause;
+            
+            try
+            {
+                lifecycle.Start();
+                Assert.IsTrue(false);
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual(cause, e);
+                Assert.AreEqual(cause, listener.Cause);
+            }
+            lifecycle.Cause = null;
+            ((StdErrLog)Log.Logger).HideStacks = false;
+            
+            
+            lifecycle.Start();
+
+            // check that the starting event has been thrown
+            Assert.IsTrue(listener.Starting, "The staring event didn't occur");
+
+
+            // check that the started event has been thrown
+            Assert.IsTrue(listener.Started, "The started event didn't occur");
+
+            // check that the starting event occurs before the started event
+            Assert.IsTrue(listener.StartingTime <= listener.StartedTime, "The starting event must occur before the started event");
+
+            // check that the lifecycle's state is started
+            Assert.IsTrue(lifecycle.IsStarted, "The lifecycle state is not started");
+        }
+
+        [Test]
+        public void TestStop()
+        {
+            TestLifeCycle lifecycle = new TestLifeCycle();
+            TestListener listener = new TestListener();
+            lifecycle.AddLifeCycleListener(listener);
+
+            
+            // need to set the state to something other than stopped or stopping or
+            // else
+            // Stop() will return without doing anything
+
+            lifecycle.Start();
+
+            ((StdErrLog)Log.Logger).HideStacks = true;
+            lifecycle.Cause = cause;
+            
+            try
+            {
+                lifecycle.Stop();
+                Assert.IsTrue(false);
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual(cause, e);
+                Assert.AreEqual(cause, listener.Cause);
+            }
+
+            
+            lifecycle.Cause = null;
+            ((StdErrLog)Log.Logger).HideStacks = false;
+            
+            lifecycle.Stop();
+
+            // check that the stopping event has been thrown
+            Assert.IsTrue(listener.Stopping, "The stopping event didn't occur");
+
+            // check that the stopped event has been thrown
+            Assert.IsTrue(listener.Stopped, "The stopped event didn't occur");
+
+            // check that the stopping event occurs before the stopped event
+            Assert.IsTrue(listener.StoppingTime <= listener.StoppedTime, "The stopping event must occur before the stopped event");
+            
+            // check that the lifecycle's state is stopped
+            Assert.IsTrue(lifecycle.IsStopped, "The lifecycle state is not stooped");
+        }
+
+
+
+    }
+}
---
+++ NJetty.Util.Test/Component/TestListener.cs
@@ -0,0 +1,163 @@
+﻿#region Apache License
+//
+// Licensed to the Apache Software Foundation (ASF) under one or more 
+// contributor license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership. 
+// The ASF licenses this file to you under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJetty.Util.Component;
+
+
+namespace NJetty.Util.Test.Component
+{
+    /// <summary>
+    /// Mock Listener
+    /// </summary>
+    /// <author>  
+    ///     <a href="mailto:leopoldo.agdeppa@gmail.com">Leopoldo Lee Agdeppa III</a>
+    /// </author>
+    /// <date>
+    /// November 2008
+    /// </date>
+    
+
+    public class TestListener : IListener
+    {
+        bool failure = false;
+        public bool Failure
+        {
+            get { return failure; }
+        }
+
+        bool started = false;
+        public bool Started
+        {
+            get { return started; }
+        }
+
+        bool starting = false;
+        public bool Starting
+        {
+            get { return starting; }
+        }
+
+
+        bool stopped = false;
+        public bool Stopped
+        {
+            get { return stopped; }
+        }
+
+
+        bool stopping = false;
+        public bool Stopping
+        {
+            get { return stopping; }
+        }
+
+        long startedTime;
+
+        public long StartedTime
+        {
+            get { return startedTime; }
+        }
+
+        long startingTime;
+        public long StartingTime
+        {
+            get { return startingTime; }
+        }
+
+        long stoppedTime;
+        public long StoppedTime
+        {
+            get { return stoppedTime; }
+        }
+
+        long stoppingTime;
+        public long StoppingTime
+        {
+            get { return stoppingTime; }
+        }
+
+        private Exception cause = null;
+
+        public Exception Cause
+        {
+            get { return cause; }
+        }
+
+
+        #region IListener Members
+
+        public void LifeCycleStarting(ILifeCycle e)
+        {
+            starting = true;
+            startingTime = DateTime.Now.ToFileTime();
+
+            // need to sleep to make sure the starting and started times are not
+            // the same
+            try
+            {
+                System.Threading.Thread.Sleep(1);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.StackTrace);
+            }
+        }
+
+        public void LifeCycleStarted(ILifeCycle e)
+        {
+            started = true;
+            startedTime = DateTime.Now.ToFileTime();
+        }
+
+        public void LifeCycleFailure(ILifeCycle e, Exception cause)
+        {
+            failure = true;
+            this.cause = cause;
+        }
+
+        public void LifeCycleStopping(ILifeCycle e)
+        {
+            stopping = true;
+            stoppingTime = DateTime.Now.ToFileTime();
+
+            // need to sleep to make sure the stopping and stopped times are not
+            // the same
+            try
+            {
+                System.Threading.Thread.Sleep(1);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.StackTrace);
+            }
+        }
+
+        public void LifeCycleStopped(ILifeCycle e)
+        {
+            stopped = true;
+            stoppedTime = DateTime.Now.ToFileTime();
+        }
+
+        #endregion
+    }
+}
