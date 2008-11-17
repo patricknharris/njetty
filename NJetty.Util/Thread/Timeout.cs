97d62779c0b65b4f0d41a9197a3fe04955e9a767
leopoldo.agdeppa <leopoldo.agdeppa@39fb20ce-a4f3-11dd-9b86-41075523de00>
2008-11-17 05:51:30 +0000

    Time out thread done

--- NJetty.Util/Thread/Timeout.cs
+++ NJetty.Util/Thread/Timeout.cs
@@ -22,6 +22,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NJetty.Util.Logger;
 
 namespace NJetty.Util.Thread
 {
@@ -37,5 +38,215 @@ namespace NJetty.Util.Thread
     /// </date>
     public class Timeout
     {
+
+        
+        internal object _lock;
+        long _duration;
+        internal long _now=System.DateTime.Now.ToFileTime();
+        TimeoutTask _head=new TimeoutTask();
+
+        public Timeout()
+        {
+            _lock=new object();
+            _head._timeout=this;
+        }
+
+        public Timeout(object lockObject)
+        {
+            _lock = lockObject;
+            _head._timeout=this;
+        }
+
+        
+        /// <summary>
+        /// Gets or Sets the duration 
+        /// </summary>
+ 
+        public long Duration
+        {
+            set {_duration =value;}
+            get{return _duration;}
+        }
+
+
+        public long Now
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _now;
+                }
+            }
+            set
+            {
+                lock (_lock)
+                {
+                    _now = value;
+                }
+            }
+        }
+
+        public long SetNow()
+        {
+            lock(_lock)
+            {
+                _now=System.DateTime.Now.ToFileTime();
+                return _now; 
+            }
+        }
+
+        /// <summary>
+        /// Get an expired tasks.
+        /// This is called instead of Tick() to obtain the next
+        /// expired Task, but without calling it's Task#Expire() or
+        /// Task#Expired() methods.
+        /// </summary>
+        /// <returns>the next expired task or null.</returns>
+        public TimeoutTask Expired()
+        {
+            lock (_lock)
+            {
+                long _expiry = _now-_duration;
+
+                if (_head._next!=_head)
+                {
+                    TimeoutTask task = _head._next;
+                    if (task._timestamp>_expiry)
+                        return null;
+
+                    task.Unlink();
+                    task._expired=true;
+                    return task;
+                }
+                return null;
+            }
+        }
+
+        public void Tick(long now)
+        {
+            long _expiry = -1;
+
+            TimeoutTask task=null;
+            while (true)
+            {
+                try
+                {
+                    lock (_lock)
+                    {
+                        if (_expiry==-1)
+                        {
+                            if (now!=-1)
+                                _now=now;
+                            _expiry = _now-_duration;
+                        }
+                            
+                        task= _head._next;
+                        if (task==_head || task._timestamp>_expiry)
+                            break;
+                        task.Unlink();
+                        task._expired=true;
+                        task.Expire();
+                    }
+                    
+                    task.Expired();
+                }
+                catch(Exception exception)
+                {
+                    Log.Warn(Log.EXCEPTION, exception);
+                }
+            }
+        }
+
+        public void Tick()
+        {
+            Tick(-1);
+        }
+
+        public void Schedule(TimeoutTask task)
+        {
+            Schedule(task,0L);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="task">task</param>
+        /// <param name="delay">A delay in addition to the default duration of the timeout</param>
+        public void Schedule(TimeoutTask task,long delay)
+        {
+            lock (_lock)
+            {
+                if (task._timestamp!=0)
+                {
+                    task.Unlink();
+                    task._timestamp=0;
+                }
+                task._timeout=this;
+                task._expired=false;
+                task._delay=delay;
+                task._timestamp = _now+delay;
+
+                TimeoutTask last=_head._prev;
+                while (last!=_head)
+                {
+                    if (last._timestamp <= task._timestamp)
+                        break;
+                    last=last._prev;
+                }
+                last.Link(task);
+            }
+        }
+
+
+        public void CancelAll()
+        {
+            lock (_lock)
+            {
+                _head._next=_head._prev=_head;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return _head._next == _head;
+                }
+            }
+        }
+
+        public long TimeToNext
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_head._next == _head)
+                        return -1;
+                    long to_next = _duration + _head._next._timestamp - _now;
+                    return to_next < 0 ? 0 : to_next;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder buf = new StringBuilder();
+            buf.Append(base.ToString());
+
+            TimeoutTask task = _head._next;
+            while (task != _head)
+            {
+                buf.Append("-->");
+                buf.Append(task);
+                task = task._next;
+            }
+
+            return buf.ToString();
+        }
+
     }
 }
---
+++ NJetty.Util/Thread/TimeoutTask.cs
@@ -0,0 +1,165 @@
+﻿#region Apache License
+//
+// Licensed to the Apache Software Foundation (ASF) under one or more 
+// contributor license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership. 
+// The ASF licenses this file to you under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NJetty.Util.Thread
+{
+    /// <summary>
+    /// Task.
+    /// The base class for scheduled timeouts.  This class should be
+    /// extended to implement the expire() method, which is called if the
+    /// timeout expires.
+    /// </summary>
+    /// <author>  
+    ///     <a href="mailto:leopoldo.agdeppa@gmail.com">Leopoldo Lee Agdeppa III</a>
+    /// </author>
+    /// <date>
+    /// November 2008
+    /// </date>
+
+
+    public class TimeoutTask
+    {
+        internal TimeoutTask _next;
+        internal TimeoutTask _prev;
+        internal Timeout _timeout;
+        internal long _delay;
+        internal long _timestamp=0;
+        internal bool _expired=false;
+
+        public TimeoutTask()
+        {
+            _next=_prev=this;
+        }
+
+        public long Timestamp
+        {
+            get { return _timestamp; }
+        }
+
+        public long Age
+        {
+            get
+            {
+                Timeout t = _timeout;
+                if (t != null && t.Now != 0 && _timestamp != 0)
+                    return t.Now - _timestamp;
+                return 0;
+            }
+        }
+
+        internal void Unlink()
+        {
+            _next._prev=_prev;
+            _prev._next=_next;
+            _next=_prev=this;
+            _expired=false;
+        }
+
+        internal void Link(TimeoutTask task)
+        {
+            TimeoutTask next_next = _next;
+            _next._prev=task;
+            _next=task;
+            _next._next=next_next;
+            _next._prev=this;   
+        }
+        
+        
+        /// <summary>
+        /// Schedule the task on the given timeout.
+        /// The task exiry will be called after the timeout duration. 
+        /// </summary>
+        /// <param name="timer"></param>
+        public void Schedule(Timeout timer)
+        {
+            timer.Schedule(this);
+        }
+        
+        
+        /// <summary>
+        /// Schedule the task on the given timeout.
+        /// The task exiry will be called after the timeout duration.
+        /// </summary>
+        /// <param name="timer"></param>
+        /// <param name="delay"></param>
+        public void Schedule(Timeout timer, long delay)
+        {
+            timer.Schedule(this,delay);
+        }
+        
+        
+        /// <summary>
+        /// Reschedule the task on the current timeout.
+        /// The task timeout is rescheduled as if it had been cancelled and
+        /// scheduled on the current timeout. 
+        /// </summary>
+        public void Reschedule()
+        {
+            Timeout timeout = _timeout;
+            if (timeout!=null)
+                timeout.Schedule(this,_delay);
+        }
+        
+        /// <summary>
+        /// Cancel the task.
+        /// Remove the task from the timeout.
+        /// </summary>
+        public void Cancel()
+        {
+            Timeout timeout = _timeout;
+            if (timeout!=null)
+            {
+                lock (timeout._lock)
+                {
+                    Unlink();
+                    _timestamp=0;
+                }
+            }
+        }
+        
+        public bool IsExpired 
+        { 
+            get { return _expired; } 
+        }
+
+        public bool IsScheduled { get { return _next != this; } }
+        
+        string a;
+        /// <summary>
+        /// Expire task.
+        /// This method is called when the timeout expires. It is called
+        /// in the scope of the lock block (on this) that sets 
+        /// the property IsExpired state to true.
+        /// </summary>
+        public virtual void Expire(){}
+
+        /// <summary>
+        /// Expire task. This method is called when the timeout expires. 
+        /// It is called outside of any synchronization scope and may be delayed. 
+        /// </summary>
+        public virtual void Expired() { }
+
+    }
+}
