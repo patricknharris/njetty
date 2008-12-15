7faec5cfc4927144c843fd1999fca70d0e925ca8
leopoldo.agdeppa <leopoldo.agdeppa@39fb20ce-a4f3-11dd-9b86-41075523de00>
2008-12-15 06:43:54 +0000

    version changed to 0.1.0.0

--- NJetty.Start/Properties/AssemblyInfo.cs
+++ NJetty.Start/Properties/AssemblyInfo.cs
@@ -39,9 +39,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NJetty.Start")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Exist")]
+[assembly: AssemblyCompany("Bong")]
 [assembly: AssemblyProduct("NJetty.Start")]
-[assembly: AssemblyCopyright("Copyright © Exist 2008")]
+[assembly: AssemblyCopyright("Copyright © Bong 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -63,5 +63,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
