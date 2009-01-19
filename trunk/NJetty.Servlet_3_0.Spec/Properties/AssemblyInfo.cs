daed59aff1cecd084694bee7920446b156a7e140
leopoldo.agdeppa <leopoldo.agdeppa@39fb20ce-a4f3-11dd-9b86-41075523de00>
2009-01-19 04:40:35 +0000

    Added Servlet 3.0 specification port to dotnet

---
+++ trunk/NJetty.Servlet_3_0.Spec/Properties/AssemblyInfo.cs
@@ -0,0 +1,68 @@
+﻿#region Apache License
+//
+// Licensed to the Apache Software Foundation (ASF) under one or more 
+// contributor license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership. 
+// The ASF licenses this file to you under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with 
+// the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+
+/// <summary>
+/// NJetty.Servlet_3_0.Spec Assembly Info
+/// Servlet 3.0 Specification Port to Dotnet
+/// </summary>
+/// <author>  
+///     <a href="mailto:leopoldo.agdeppa@gmail.com">Leopoldo Lee Agdeppa III</a>
+/// </author>
+/// <date>
+/// November 2008
+/// </date>
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NJetty.Servlet_3_0.Spec")]
+[assembly: AssemblyDescription("Java Servlet 3.0 Port to Dotnet")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bong")]
+[assembly: AssemblyProduct("NJetty.Servlet_3_0.Spec")]
+[assembly: AssemblyCopyright("Copyright © Bong 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("29ccdd7e-7ad1-41ac-b65f-9f097961433e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
