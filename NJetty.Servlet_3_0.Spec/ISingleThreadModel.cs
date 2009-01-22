6b632bfda4aed6ee5e954e5a4372bde6d05f79fb
leopoldo.agdeppa <leopoldo.agdeppa@39fb20ce-a4f3-11dd-9b86-41075523de00>
2009-01-22 04:16:09 +0000

    ISingleThreadModel done

--- NJetty.Servlet_3_0.Spec/ISingleThreadModel.cs
+++ NJetty.Servlet_3_0.Spec/ISingleThreadModel.cs
@@ -26,14 +26,33 @@ namespace Javax.NServlet
 {
 
     /// <summary>
-    /// TODO: Class/Interface Information here
+    /// Ensures that servlets handle
+    /// only one request at a time. This interface has no methods.
+    /// 
+    /// If a servlet implements this interface, you are <i>guaranteed</i>
+    /// that no two threads will execute concurrently in the
+    /// servlet's <code>service</code> method. The servlet container
+    /// can make this guarantee by synchronizing access to a single
+    /// instance of the servlet, or by maintaining a pool of servlet
+    /// instances and dispatching each new request to a free servlet.
+    /// 
+    /// Note that SingleThreadModel does not solve all thread safety
+    /// issues.  For example, session attributes and static variables can
+    /// still be accessed by multiple requests on multiple threads
+    /// at the same time, even when SingleThreadModel servlets are used.
+    /// It is recommended that a developer take other means to resolve
+    /// those issues instead of implementing this interface, such as
+    /// avoiding the usage of an instance variable or synchronizing
+    /// the block of the code accessing those resources.
+    /// This interface is deprecated in Servlet API version 2.4.
     /// </summary>
     /// <author>  
     ///     <a href="mailto:leopoldo.agdeppa@gmail.com">Leopoldo Lee Agdeppa III</a>
     /// </author>
     /// <date>
-    /// TODO: date here
+    /// January 2009
     /// </date>
+    [Obsolete("As of Java Servlet API 2.4, with no direct replacement.")]
     public interface ISingleThreadModel
     {
     }
