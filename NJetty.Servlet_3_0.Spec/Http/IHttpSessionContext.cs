962b022e89e60fc7ca98acee7305d8a1fd638ee3
leopoldo.agdeppa <leopoldo.agdeppa@39fb20ce-a4f3-11dd-9b86-41075523de00>
2009-04-28 05:51:02 +0000

    IHttpSessionContext implemented and done

--- NJetty.Servlet_3_0.Spec/Http/IHttpSessionContext.cs
+++ NJetty.Servlet_3_0.Spec/Http/IHttpSessionContext.cs
@@ -21,20 +21,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace Javax.NServlet.Http
 {
 
     /// <summary>
-    /// TODO: Class/Interface Information here
+    /// Obsolete As of Java(tm) Servlet API 2.1 for security reasons, 
+    /// with no replacement. This interface will be removed in a future 
+    /// version of this API.
     /// </summary>
     /// <author>  
     ///     <a href="mailto:leopoldo.agdeppa@gmail.com">Leopoldo Lee Agdeppa III</a>
     /// </author>
     /// <date>
-    /// TODO: date here
+    /// April 28, 2009
     /// </date>
+    [Obsolete("As of Java(tm) Servlet API 2.1 for security reasons, with no replacement. This interface will be removed in a future version of this API.")]
     public interface IHttpSessionContext
     {
+      
+
+
+        /// <summary>
+        /// Obsolete As of Java Servlet API 2.1 with
+        /// no replacement. This method must 
+        /// return null and will be removed in
+        /// a future version of this API.
+        /// </summary>
+        /// <param name="sessionId"></param>
+        /// <returns></returns>
+        [Obsolete("As of Java Servlet API 2.1 with no replacement. This method must return null and will be removed in a future version of this API.")]
+        IHttpSession GetSession(String sessionId);
+
+
+
+
+        /// <summary>
+        /// Obsolete as of Java Servlet API 2.1 with no replacement. 
+        /// This method must return an empty List and will be removed 
+        /// in a future version of this API.
+        /// </summary>
+        [Obsolete("As of Java Servlet API 2.1 with no replacement. This method must return an empty List and will be removed in a future version of this API.")]
+        IList Ids
+        {
+            get;
+        }
     }
 }
